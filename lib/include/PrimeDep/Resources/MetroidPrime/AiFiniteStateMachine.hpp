69be413ae49291618b5c041239b5e000a7e0ab92
Phillip Stephens <antidote.crk@gmail.com>
2025-04-27 21:50:41 -0700

    Add AiFiniteStateMachine uncooking

--- lib/include/PrimeDep/Resources/MetroidPrime/AiFiniteStateMachine.hpp
+++ lib/include/PrimeDep/Resources/MetroidPrime/AiFiniteStateMachine.hpp
@@ -5,7 +5,61 @@ namespace axdl::primedep::MetroidPrime {
 class AiFiniteStateMachine final
 : public TypedResource('AFSM', ".statemachine", ".afsm", DESCRIPTION("Ai Finite State Machine")) {
 public:
+  class Trigger {
+  public:
+    explicit Trigger(athena::io::IStreamReader& in, bool first);
+
+    [[nodiscard]] const std::string& name() const { return m_name; }
+    void setName(const std::string& name) { m_name = name; }
+
+    [[nodiscard]] float parameter() const { return m_parameter; }
+    void setParameter(const float parameter) { m_parameter = parameter; }
+
+    [[nodiscard]] uint32_t targetState() const { return m_targetState; }
+    void setTargetState(const uint32_t state) { m_targetState = state; }
+
+    void PutTo(nlohmann::ordered_json& out, bool first) const;
+
+  private:
+    std::string m_name;
+    float m_parameter{};
+    uint32_t m_targetState{};
+  };
+
+  class Transition {
+  public:
+    explicit Transition(athena::io::IStreamReader& in);
+
+    [[nodiscard]] const std::vector<Trigger>& triggers() const { return m_triggers; }
+    void setTransitions(const std::vector<Trigger>& triggers) { m_triggers = triggers; }
+
+    void PutTo(nlohmann::ordered_json& out) const;
+
+  private:
+    std::vector<Trigger> m_triggers;
+  };
+
+  class State {
+  public:
+    explicit State(athena::io::IStreamReader& in);
+
+    [[nodiscard]] const std::string& name() const { return m_name; }
+    void setName(const std::string& name) { m_name = name; }
+
+    [[nodiscard]] const std::vector<Transition>& transitions() const { return m_transitions; }
+    void setTransitions(const std::vector<Transition>& transitions) { m_transitions = transitions; }
+
+    void PutTo(nlohmann::ordered_json& out) const;
+
+  private:
+    std::string m_name;
+    std::vector<Transition> m_transitions;
+  };
+
   AiFiniteStateMachine(const char* ptr, std::size_t size);
+
+  bool writeUncooked(std::string_view path) const override;
+
   static std::shared_ptr<IResource> loadCooked(const char* ptr, std::size_t size);
 
   static bool canIngest(const nlohmann::ordered_json& metadata) {
@@ -14,5 +68,8 @@ public:
   static std::shared_ptr<IResource> ingest(const nlohmann::ordered_json& metadata, std::string_view repPath) {
     return nullptr;
   }
+
+private:
+  std::vector<State> m_states;
 };
 } // namespace axdl::primedep::MetroidPrime
--- lib/src/PrimeDep/Resources/MetroidPrime/AiFiniteStateMachine.cpp
+++ lib/src/PrimeDep/Resources/MetroidPrime/AiFiniteStateMachine.cpp
@@ -3,9 +3,85 @@
 #include "athena/MemoryReader.hpp"
 
 namespace axdl::primedep::MetroidPrime {
-AiFiniteStateMachine::AiFiniteStateMachine(const char* ptr, const std::size_t size) {
-  athena::io::MemoryReader mr(ptr, size, true);
+AiFiniteStateMachine::Trigger::Trigger(athena::io::IStreamReader& in, const bool first)
+: m_name(in.readString()), m_parameter(in.readFloatBig()) {
+  if (first) {
+    m_targetState = in.readUint32Big();
+  }
 }
+void AiFiniteStateMachine::Trigger::PutTo(nlohmann::ordered_json& out, const bool first) const {
+  out["Name"] = m_name;
+  out["Parameter"] = m_parameter;
+  if (first) {
+    out["TargetState"] = m_targetState;
+  }
+}
+
+AiFiniteStateMachine::Transition::Transition(athena::io::IStreamReader& in) {
+  auto triggerCount = in.readUint32Big();
+  bool first = true;
+  while (triggerCount--) {
+    m_triggers.emplace_back(in, first);
+    first = false;
+  }
+}
+
+void AiFiniteStateMachine::Transition::PutTo(nlohmann::ordered_json& out) const {
+  auto& triggers = out["Triggers"];
+  bool first = true;
+  for (const auto& trigger : m_triggers) {
+    trigger.PutTo(triggers.emplace_back(), first);
+    first = false;
+  }
+}
+
+AiFiniteStateMachine::State::State(athena::io::IStreamReader& in) {
+  auto transitionCount = in.readUint32Big();
+  while (transitionCount--) {
+    m_transitions.emplace_back(in);
+  }
+}
+
+void AiFiniteStateMachine::State::PutTo(nlohmann::ordered_json& out) const {
+  out["Name"] = m_name;
+  auto& transitions = out["Transitions"];
+  for (const auto& transition : m_transitions) {
+    transition.PutTo(transitions.emplace_back());
+  }
+}
+
+AiFiniteStateMachine::AiFiniteStateMachine(const char* ptr, const std::size_t size) {
+  athena::io::MemoryReader in(ptr, size, true);
+  const auto stateCount = in.readUint32Big();
+  std::vector<std::string> names;
+
+  for (int i = 0; i < stateCount; i++) {
+    names.push_back(in.readString());
+  }
+
+  in.readUint32Big(); // Total number of triggers in this state machine
+
+  for (int i = 0; i < stateCount; i++) {
+    auto& state = m_states.emplace_back(in);
+    state.setName(names[i]);
+  }
+}
+
+bool AiFiniteStateMachine::writeUncooked(const std::string_view path) const {
+  const auto p = rawPath(path);
+
+  nlohmann::ordered_json j;
+  auto& states = j["States"];
+  for (const auto& state : m_states) {
+    state.PutTo(states.emplace_back());
+  }
+
+  athena::io::FileWriter writer(p.generic_string());
+  const auto js = j.dump(4) + "\n";
+  writer.writeString(js, js.length());
+  return !writer.hasError();
+}
+
 std::shared_ptr<IResource> AiFiniteStateMachine::loadCooked(const char* ptr, std::size_t size) {
   return std::make_shared<AiFiniteStateMachine>(ptr, size);
 }
